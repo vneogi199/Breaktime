abad42fff1a3080fced70ff3bd61a4af64c1fc0d
Vinit Neogi <vneogi199@gmail.com>
2017-07-15 21:33:52 +0530

    Add files via upload

---
+++ Breaktime.py
@@ -0,0 +1,7 @@
+import webbrowser
+import time
+
+while(True):
+    time.sleep(2*60*60)
+    webbrowser.open("http://wp.auburn.edu/ahmrei/wp-content/uploads/2015/12/take-a-break-to-start-again.jpg")
+ 
